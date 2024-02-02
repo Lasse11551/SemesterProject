7729789dd26162f2f3c2cdd469622517b2b6077f
Lasse Lysskov <56248206+Lasse11551@users.noreply.github.com>
2024-02-02 09:39:38 +0100

    Created variables file

---
+++ 01._Miscellaneous/variables_I.js
@@ -0,0 +1,22 @@
+console.log("Hello World");
+//We will always default to using const
+const myName = "Lasse";
+
+const me = {};
+me.name = myName;
+console.log(me);
+//const is constant NOT in the value but in the assignment
+//meaning that it cannot be reassigned and thus also HAS to be assigned
+
+//SyntaxError: Missing initializer in const desclaration
+//const test;
+
+const hobbies = ["coding", "eating"];
+
+hobbies.push("sleeping");
+
+//hobbies = [];
+//hobbies = ""; man kan ikke ændre datatypen på en const 
+
+me.hobbies = hobbies;
+console.log(me);
