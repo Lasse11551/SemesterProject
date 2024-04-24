44b4d338a1bfdc05258ad059c086cbe11028bfdb
Lasse Lysskov <56248206+Lasse11551@users.noreply.github.com>
2024-04-24 10:37:02 +0200

    SetupMandatory2

---
+++ 12._Mandatory2/server/app.js
@@ -0,0 +1,7 @@
+import "dotenv/config"
+
+import express from "express"
+const app = express();
+
+const PORT = process.env.PORT ?? 8080;
+app.listen(PORT, () => console.log("Server is running on port", 8080))
